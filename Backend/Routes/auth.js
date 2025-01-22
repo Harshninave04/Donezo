4c22f5af897b38891a1dd38baf6214c2ab3727a5
Harsh Ninave <harshninave58@gmail.com>
2025-01-22 23:12:50 +0530

    mongoose initialized, Schema created!!

---
+++ Backend/Routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('../Models/User');
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    // Check if User already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
+    // Create new User
+    const user = new User({ username, email, password });
+    await user.save();
+
+    // Generate JWT token
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+      
+      res.status(201).json({ user, token });
+  } catch (error) {
+      res.status(500).json({ message: 'Something went wrong' });
+  }
+});
+
+module.exports = router;
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 5000;
 const cors = require('cors');
+const authRoutes = require("./Routes/auth")
 
-app.use(cors())
+require('dotenv').config();
+
+// Middleware to parse JSON
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes)
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("Failed to connect to MongoDB", err));
+  
+
 
 app.get("/", (req, res) => {
     res.send("Backend is running!")
