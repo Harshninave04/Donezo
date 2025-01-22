efd048bb81c6aca1d2966df3fde23cb7c961aa2c
Harsh Ninave <harshninave58@gmail.com>
2025-01-22 20:42:07 +0530

    frontend and backend connected!!

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const cors = require('cors');
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Backend is running!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000')
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
   return (
-    <div className="bg-black text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-light">Task Manager</h1>
+    <div className="p-8">
+      <h1 className="text-2xl font-light">Welcome to Task Manager</h1>
+      <p className="mt-4 font-light bg-green-50 p-3 max-w-fit">{message}</p>
     </div>
   );
 }
-
